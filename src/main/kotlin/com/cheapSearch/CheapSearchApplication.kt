afc1fc6a277ceda197409ca9756d05b6cc2810d6
KPosysaev <KPosysaev@inno.tech>
2023-01-17 17:34:29 +0300

    Init commit

---
+++ src/main/kotlin/com/cheapSearch/CheapSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.cheapSearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CheapSearchApplication
+
+fun main(args: Array<String>) {
+	runApplication<CheapSearchApplication>(*args)
+}
